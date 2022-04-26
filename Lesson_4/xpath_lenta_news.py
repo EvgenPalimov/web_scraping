69546a3bb06a93cdb9fc578729ee1d468822ee7b
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-04-27 01:05:29 +0500

    HomeWork_4 lenta_news is ready v.2

--- Lesson_4/xpath_lenta_news.py
+++ Lesson_4/xpath_lenta_news.py
@@ -6,56 +6,61 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv("../.env")
-MONGO_HOST = os.getenv("MONGO_HOST", None)
-MONGO_PORT = int(os.getenv("MONGO_PORT", None))
-MONGO_DB = os.getenv("MONGO_DB", None)
-MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", None)
-
-URL = 'https://lenta.ru'
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/98.0.4758.80 Safari/537.36",
-}
-
-ITEMS_XPATH = '//div[@class="last24"]//a'
-TITLE_XPATH = 'Lenta.ru'
-NEWS_TITLE_XPATH = './/span[contains(@class, "__title")]//text()'
-LINK_TO_NEWS = './@href'
-DATE_OF_PUBLICATION_XPATH = '//time[contains(@class, "topic-header__item")]//text()'
 
 
-def get_date(href: str):
-    r = requests.get(href, headers=HEADERS)
-    dom = fromstring(r.text)
-    date = dom.xpath(DATE_OF_PUBLICATION_XPATH)[0]
-    return re.sub(r'(\d.*),.', '', date)
+class News_lenta:
+
+    def __init__(self):
+        self.URL = 'https://lenta.ru'
+        self.HEADERS = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/98.0.4758.80 Safari/537.36",
+        }
+        self.ITEMS_XPATH = '//div[@class="last24"]//a'
+        self.TITLE_XPATH = 'Lenta.ru'
+        self.NEWS_TITLE_XPATH = './/span[contains(@class, "__title")]//text()'
+        self.LINK_TO_NEWS = './@href'
+        self.DATE_OF_PUBLICATION_XPATH = '//time[contains(@class, "topic-header__item")]//text()'
+        self.MONGO_HOST = os.getenv("MONGO_HOST", None)
+        self.MONGO_PORT = int(os.getenv("MONGO_PORT", None))
+        self.MONGO_DB = os.getenv("MONGO_DB", None)
+        self.MONGO_COLLECTION = os.getenv("MONGO_COLLECTION", None)
+        self.link_news = ''
+        self.news = {}
+
+    def get_date(self):
+        r = requests.get(self.link_news, headers=self.HEADERS)
+        dom = fromstring(r.text)
+        date = dom.xpath(self.DATE_OF_PUBLICATION_XPATH)[0]
+        return re.sub(r'(\d.*),.', '', date)
+
+    def get_data(self, items):
+        for item in items:
+            self.news['name_source'] = self.TITLE_XPATH
+            self.news['title_news'] = item.xpath(self.NEWS_TITLE_XPATH)[0]
+            link = item.xpath(self.LINK_TO_NEWS)[0]
+            self.link_news = f'{self.URL}{link}'
+            self.news['link_news'] = self.link_news
+            self.news['date'] = self.get_date()
+            self.write_news_to_db()
+
+    def get_html(self):
+        r = requests.get(self.URL, headers=self.HEADERS)
+        dom = fromstring(r.text)
+        items = dom.xpath(self.ITEMS_XPATH)
+        self.get_data(items)
+
+    def write_news_to_db(self):
+        with MongoClient(self.MONGO_HOST, self.MONGO_PORT) as client:
+            db = client[self.MONGO_DB]
+            collection = db[self.MONGO_COLLECTION]
+            collection.update_one(
+                {'link_news': self.link_news},
+                {'$set': self.news},
+                upsert=True,
+            )
 
 
-def write_jbos_to_db(link: str, news: dict):
-    with MongoClient(MONGO_HOST, MONGO_PORT) as client:
-        db = client[MONGO_DB]
-        collection = db[MONGO_COLLECTION]
-        collection.update_one(
-            {'link_news': link},
-            {'$set': news},
-            upsert=True,
-        )
-
-
-def get_data(items):
-    news = {}
-    for item in items:
-        news['name_source'] = TITLE_XPATH
-        news['title_news'] = item.xpath(NEWS_TITLE_XPATH)[0]
-        link = item.xpath(LINK_TO_NEWS)[0]
-        link_news = f'{URL}{link}'
-        news['link_news'] = link_news
-        news['date'] = get_date(link_news)
-        write_jbos_to_db(link_news, news)
-
-
-r = requests.get(URL, headers=HEADERS)
-dom = fromstring(r.text)
-items = dom.xpath(ITEMS_XPATH)
-get_data(items)
+start = News_lenta()
+start.get_html()
