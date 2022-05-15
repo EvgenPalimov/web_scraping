fc6049d73cff6d0a5db36cb21c620786383e89b1
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-05-15 11:57:43 +0500

    HomeWork_6 is ready v.3

--- runner.py
+++ runner.py
@@ -10,8 +10,10 @@ if __name__ == '__main__':
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
 
-    name_vacancy = input('Введите название желаемой вакансии в формате "-a query=Название вакансии"'
-                         ' или нажмите клавишу "Ввод": ')
+    # Для запуска из консоли необходимо использовать параметр
+    # "-a QUERY=VALUE" VALUE - название вакансии
+
+    name_vacancy = input('Введите название желаемой вакансии или нажмите клавишу "Ввод": ')
     if name_vacancy:
         search_kwargs = {'query': name_vacancy}
     else:
